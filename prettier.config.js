c04531c3a988551877e747c8766370628fa3e459
Shaun Saker <shaun@aux.co.za>
2019-10-04 09:51:56 +0200

    Add prettier

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  tabWidth: 2,
+  singleQuote: true,
+  printWidth: 100,
+  semi: true,
+  trailingComma: "all",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "always"
+};
