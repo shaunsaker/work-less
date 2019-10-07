aaf262a1b413b6779d65abbec4de0d5f4c6907f5
Shaun Saker <shaun@aux.co.za>
2019-10-07 07:38:10 +0200

    Storybook/Fix custom fonts

--- packages/app/.storybook/webpack.config.js
+++ packages/app/.storybook/webpack.config.js
@@ -1,11 +1,20 @@
+const path = require('path');
+
 module.exports = ({ config, mode }) => {
-  config.module.rules.push({
-    test: /\.(ts|tsx)$/,
-    loader: require.resolve("babel-loader"),
-    options: {
-      presets: [["react-app", { flow: false, typescript: true }]]
-    }
-  });
-  config.resolve.extensions.push(".ts", ".tsx");
+  config.module.rules.push(
+    {
+      test: /\.(ts|tsx)$/,
+      loader: require.resolve('babel-loader'),
+      options: {
+        presets: [['react-app', { flow: false, typescript: true }]],
+      },
+    },
+    {
+      test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+      loaders: ['file-loader'],
+      include: path.resolve(__dirname, '../'),
+    },
+  );
+  config.resolve.extensions.push('.ts', '.tsx');
   return config;
 };
