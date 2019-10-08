c14b581394e048082aadfd30607a26674d98640c
Shaun Saker <shaun@aux.co.za>
2019-10-08 10:00:10 +0200

    App/Add country images

---
+++ packages/app/src/assets/images/countries/index.ts
@@ -0,0 +1,505 @@
+const adImage = require('./ad.png');
+const aeImage = require('./ae.png');
+const afImage = require('./af.png');
+const agImage = require('./ag.png');
+const aiImage = require('./ai.png');
+const alImage = require('./al.png');
+const amImage = require('./am.png');
+const anImage = require('./an.png');
+const aoImage = require('./ao.png');
+const aqImage = require('./aq.png');
+const arImage = require('./ar.png');
+const asImage = require('./as.png');
+const atImage = require('./at.png');
+const auImage = require('./au.png');
+const awImage = require('./aw.png');
+const axImage = require('./ax.png');
+const azImage = require('./az.png');
+const baImage = require('./ba.png');
+const bbImage = require('./bb.png');
+const bdImage = require('./bd.png');
+const beImage = require('./be.png');
+const bfImage = require('./bf.png');
+const bgImage = require('./bg.png');
+const bhImage = require('./bh.png');
+const biImage = require('./bi.png');
+const bjImage = require('./bj.png');
+const blImage = require('./bl.png');
+const bmImage = require('./bm.png');
+const bnImage = require('./bn.png');
+const boImage = require('./bo.png');
+const bqImage = require('./bq.png');
+const brImage = require('./br.png');
+const bsImage = require('./bs.png');
+const btImage = require('./bt.png');
+const bvImage = require('./bv.png');
+const bwImage = require('./bw.png');
+const byImage = require('./by.png');
+const bzImage = require('./bz.png');
+const caImage = require('./ca.png');
+const ccImage = require('./cc.png');
+const cdImage = require('./cd.png');
+const cfImage = require('./cf.png');
+const cgImage = require('./cg.png');
+const chImage = require('./ch.png');
+const ciImage = require('./ci.png');
+const ckImage = require('./ck.png');
+const clImage = require('./cl.png');
+const cmImage = require('./cm.png');
+const cnImage = require('./cn.png');
+const coImage = require('./co.png');
+const crImage = require('./cr.png');
+const cuImage = require('./cu.png');
+const cvImage = require('./cv.png');
+const cwImage = require('./cw.png');
+const cxImage = require('./cx.png');
+const cyImage = require('./cy.png');
+const czImage = require('./cz.png');
+const deImage = require('./de.png');
+const djImage = require('./dj.png');
+const dkImage = require('./dk.png');
+const dmImage = require('./dm.png');
+const dzImage = require('./dz.png');
+const ecImage = require('./ec.png');
+const eeImage = require('./ee.png');
+const egImage = require('./eg.png');
+const ehImage = require('./eh.png');
+const erImage = require('./er.png');
+const esImage = require('./es.png');
+const etImage = require('./et.png');
+const fiImage = require('./fi.png');
+const fjImage = require('./fj.png');
+const fkImage = require('./fk.png');
+const fmImage = require('./fm.png');
+const foImage = require('./fo.png');
+const frImage = require('./fr.png');
+const gaImage = require('./ga.png');
+const gbImage = require('./gb.png');
+const gdImage = require('./gd.png');
+const geImage = require('./ge.png');
+const gfImage = require('./gf.png');
+const ggImage = require('./gg.png');
+const ghImage = require('./gh.png');
+const giImage = require('./gi.png');
+const glImage = require('./gl.png');
+const gmImage = require('./gm.png');
+const gnImage = require('./gn.png');
+const gpImage = require('./gp.png');
+const gqImage = require('./gq.png');
+const grImage = require('./gr.png');
+const gsImage = require('./gs.png');
+const gtImage = require('./gt.png');
+const guImage = require('./gu.png');
+const gwImage = require('./gw.png');
+const gyImage = require('./gy.png');
+const hkImage = require('./hk.png');
+const hmImage = require('./hm.png');
+const hnImage = require('./hn.png');
+const hrImage = require('./hr.png');
+const htImage = require('./ht.png');
+const huImage = require('./hu.png');
+const idImage = require('./id.png');
+const ieImage = require('./ie.png');
+const ilImage = require('./il.png');
+const imImage = require('./im.png');
+const inImage = require('./in.png');
+const ioImage = require('./io.png');
+const iqImage = require('./iq.png');
+const irImage = require('./ir.png');
+const isImage = require('./is.png');
+const itImage = require('./it.png');
+const jeImage = require('./je.png');
+const jmImage = require('./jm.png');
+const joImage = require('./jo.png');
+const jpImage = require('./jp.png');
+const keImage = require('./ke.png');
+const kgImage = require('./kg.png');
+const khImage = require('./kh.png');
+const kiImage = require('./ki.png');
+const kmImage = require('./km.png');
+const knImage = require('./kn.png');
+const kpImage = require('./kp.png');
+const krImage = require('./kr.png');
+const kwImage = require('./kw.png');
+const kyImage = require('./ky.png');
+const kzImage = require('./kz.png');
+const laImage = require('./la.png');
+const lbImage = require('./lb.png');
+const lcImage = require('./lc.png');
+const liImage = require('./li.png');
+const lkImage = require('./lk.png');
+const lrImage = require('./lr.png');
+const lsImage = require('./ls.png');
+const ltImage = require('./lt.png');
+const luImage = require('./lu.png');
+const lvImage = require('./lv.png');
+const lyImage = require('./ly.png');
+const maImage = require('./ma.png');
+const mcImage = require('./mc.png');
+const mdImage = require('./md.png');
+const meImage = require('./me.png');
+const mfImage = require('./mf.png');
+const mgImage = require('./mg.png');
+const mhImage = require('./mh.png');
+const mkImage = require('./mk.png');
+const mlImage = require('./ml.png');
+const mmImage = require('./mm.png');
+const mnImage = require('./mn.png');
+const moImage = require('./mo.png');
+const mpImage = require('./mp.png');
+const mqImage = require('./mq.png');
+const mrImage = require('./mr.png');
+const msImage = require('./ms.png');
+const mtImage = require('./mt.png');
+const muImage = require('./mu.png');
+const mvImage = require('./mv.png');
+const mwImage = require('./mw.png');
+const mxImage = require('./mx.png');
+const myImage = require('./my.png');
+const mzImage = require('./mz.png');
+const naImage = require('./na.png');
+const ncImage = require('./nc.png');
+const neImage = require('./ne.png');
+const nfImage = require('./nf.png');
+const ngImage = require('./ng.png');
+const niImage = require('./ni.png');
+const nlImage = require('./nl.png');
+const noImage = require('./no.png');
+const npImage = require('./np.png');
+const nrImage = require('./nr.png');
+const nuImage = require('./nu.png');
+const nzImage = require('./nz.png');
+const omImage = require('./om.png');
+const paImage = require('./pa.png');
+const peImage = require('./pe.png');
+const pfImage = require('./pf.png');
+const pgImage = require('./pg.png');
+const phImage = require('./ph.png');
+const pkImage = require('./pk.png');
+const plImage = require('./pl.png');
+const pmImage = require('./pm.png');
+const pnImage = require('./pn.png');
+const prImage = require('./pr.png');
+const psImage = require('./ps.png');
+const ptImage = require('./pt.png');
+const pwImage = require('./pw.png');
+const pyImage = require('./py.png');
+const qaImage = require('./qa.png');
+const reImage = require('./re.png');
+const roImage = require('./ro.png');
+const rsImage = require('./rs.png');
+const ruImage = require('./ru.png');
+const rwImage = require('./rw.png');
+const saImage = require('./sa.png');
+const sbImage = require('./sb.png');
+const scImage = require('./sc.png');
+const sdImage = require('./sd.png');
+const seImage = require('./se.png');
+const sgImage = require('./sg.png');
+const shImage = require('./sh.png');
+const siImage = require('./si.png');
+const sjImage = require('./sj.png');
+const skImage = require('./sk.png');
+const slImage = require('./sl.png');
+const smImage = require('./sm.png');
+const snImage = require('./sn.png');
+const soImage = require('./so.png');
+const srImage = require('./sr.png');
+const ssImage = require('./ss.png');
+const stImage = require('./st.png');
+const svImage = require('./sv.png');
+const sxImage = require('./sx.png');
+const syImage = require('./sy.png');
+const szImage = require('./sz.png');
+const tcImage = require('./tc.png');
+const tdImage = require('./td.png');
+const tfImage = require('./tf.png');
+const tgImage = require('./tg.png');
+const thImage = require('./th.png');
+const tjImage = require('./tj.png');
+const tkImage = require('./tk.png');
+const tlImage = require('./tl.png');
+const tmImage = require('./tm.png');
+const tnImage = require('./tn.png');
+const toImage = require('./to.png');
+const trImage = require('./tr.png');
+const ttImage = require('./tt.png');
+const tvImage = require('./tv.png');
+const twImage = require('./tw.png');
+const tzImage = require('./tz.png');
+const uaImage = require('./ua.png');
+const ugImage = require('./ug.png');
+const umImage = require('./um.png');
+const usImage = require('./us.png');
+const uyImage = require('./uy.png');
+const uzImage = require('./uz.png');
+const vaImage = require('./va.png');
+const vcImage = require('./vc.png');
+const veImage = require('./ve.png');
+const vgImage = require('./vg.png');
+const viImage = require('./vi.png');
+const vnImage = require('./vn.png');
+const vuImage = require('./vu.png');
+const wfImage = require('./wf.png');
+const wsImage = require('./ws.png');
+const xkImage = require('./xk.png');
+const yeImage = require('./ye.png');
+const ytImage = require('./yt.png');
+const zaImage = require('./za.png');
+const zmImage = require('./zm.png');
+const zwImage = require('./zw.png');
+
+const countries = {
+  ad: adImage,
+  ae: aeImage,
+  af: afImage,
+  ag: agImage,
+  ai: aiImage,
+  al: alImage,
+  am: amImage,
+  an: anImage,
+  ao: aoImage,
+  aq: aqImage,
+  ar: arImage,
+  as: asImage,
+  at: atImage,
+  au: auImage,
+  aw: awImage,
+  ax: axImage,
+  az: azImage,
+  ba: baImage,
+  bb: bbImage,
+  bd: bdImage,
+  be: beImage,
+  bf: bfImage,
+  bg: bgImage,
+  bh: bhImage,
+  bi: biImage,
+  bj: bjImage,
+  bl: blImage,
+  bm: bmImage,
+  bn: bnImage,
+  bo: boImage,
+  bq: bqImage,
+  br: brImage,
+  bs: bsImage,
+  bt: btImage,
+  bv: bvImage,
+  bw: bwImage,
+  by: byImage,
+  bz: bzImage,
+  ca: caImage,
+  cc: ccImage,
+  cd: cdImage,
+  cf: cfImage,
+  cg: cgImage,
+  ch: chImage,
+  ci: ciImage,
+  ck: ckImage,
+  cl: clImage,
+  cm: cmImage,
+  cn: cnImage,
+  co: coImage,
+  cr: crImage,
+  cu: cuImage,
+  cv: cvImage,
+  cw: cwImage,
+  cx: cxImage,
+  cy: cyImage,
+  cz: czImage,
+  de: deImage,
+  dj: djImage,
+  dk: dkImage,
+  dm: dmImage,
+  dz: dzImage,
+  ec: ecImage,
+  ee: eeImage,
+  eg: egImage,
+  eh: ehImage,
+  er: erImage,
+  es: esImage,
+  et: etImage,
+  fi: fiImage,
+  fj: fjImage,
+  fk: fkImage,
+  fm: fmImage,
+  fo: foImage,
+  fr: frImage,
+  ga: gaImage,
+  gb: gbImage,
+  gd: gdImage,
+  ge: geImage,
+  gf: gfImage,
+  gg: ggImage,
+  gh: ghImage,
+  gi: giImage,
+  gl: glImage,
+  gm: gmImage,
+  gn: gnImage,
+  gp: gpImage,
+  gq: gqImage,
+  gr: grImage,
+  gs: gsImage,
+  gt: gtImage,
+  gu: guImage,
+  gw: gwImage,
+  gy: gyImage,
+  hk: hkImage,
+  hm: hmImage,
+  hn: hnImage,
+  hr: hrImage,
+  ht: htImage,
+  hu: huImage,
+  id: idImage,
+  ie: ieImage,
+  il: ilImage,
+  im: imImage,
+  in: inImage,
+  io: ioImage,
+  iq: iqImage,
+  ir: irImage,
+  is: isImage,
+  it: itImage,
+  je: jeImage,
+  jm: jmImage,
+  jo: joImage,
+  jp: jpImage,
+  ke: keImage,
+  kg: kgImage,
+  kh: khImage,
+  ki: kiImage,
+  km: kmImage,
+  kn: knImage,
+  kp: kpImage,
+  kr: krImage,
+  kw: kwImage,
+  ky: kyImage,
+  kz: kzImage,
+  la: laImage,
+  lb: lbImage,
+  lc: lcImage,
+  li: liImage,
+  lk: lkImage,
+  lr: lrImage,
+  ls: lsImage,
+  lt: ltImage,
+  lu: luImage,
+  lv: lvImage,
+  ly: lyImage,
+  ma: maImage,
+  mc: mcImage,
+  md: mdImage,
+  me: meImage,
+  mf: mfImage,
+  mg: mgImage,
+  mh: mhImage,
+  mk: mkImage,
+  ml: mlImage,
+  mm: mmImage,
+  mn: mnImage,
+  mo: moImage,
+  mp: mpImage,
+  mq: mqImage,
+  mr: mrImage,
+  ms: msImage,
+  mt: mtImage,
+  mu: muImage,
+  mv: mvImage,
+  mw: mwImage,
+  mx: mxImage,
+  my: myImage,
+  mz: mzImage,
+  na: naImage,
+  nc: ncImage,
+  ne: neImage,
+  nf: nfImage,
+  ng: ngImage,
+  ni: niImage,
+  nl: nlImage,
+  no: noImage,
+  np: npImage,
+  nr: nrImage,
+  nu: nuImage,
+  nz: nzImage,
+  om: omImage,
+  pa: paImage,
+  pe: peImage,
+  pf: pfImage,
+  pg: pgImage,
+  ph: phImage,
+  pk: pkImage,
+  pl: plImage,
+  pm: pmImage,
+  pn: pnImage,
+  pr: prImage,
+  ps: psImage,
+  pt: ptImage,
+  pw: pwImage,
+  py: pyImage,
+  qa: qaImage,
+  re: reImage,
+  ro: roImage,
+  rs: rsImage,
+  ru: ruImage,
+  rw: rwImage,
+  sa: saImage,
+  sb: sbImage,
+  sc: scImage,
+  sd: sdImage,
+  se: seImage,
+  sg: sgImage,
+  sh: shImage,
+  si: siImage,
+  sj: sjImage,
+  sk: skImage,
+  sl: slImage,
+  sm: smImage,
+  sn: snImage,
+  so: soImage,
+  sr: srImage,
+  ss: ssImage,
+  st: stImage,
+  sv: svImage,
+  sx: sxImage,
+  sy: syImage,
+  sz: szImage,
+  tc: tcImage,
+  td: tdImage,
+  tf: tfImage,
+  tg: tgImage,
+  th: thImage,
+  tj: tjImage,
+  tk: tkImage,
+  tl: tlImage,
+  tm: tmImage,
+  tn: tnImage,
+  to: toImage,
+  tr: trImage,
+  tt: ttImage,
+  tv: tvImage,
+  tw: twImage,
+  tz: tzImage,
+  ua: uaImage,
+  ug: ugImage,
+  um: umImage,
+  us: usImage,
+  uy: uyImage,
+  uz: uzImage,
+  va: vaImage,
+  vc: vcImage,
+  ve: veImage,
+  vg: vgImage,
+  vi: viImage,
+  vn: vnImage,
+  vu: vuImage,
+  wf: wfImage,
+  ws: wsImage,
+  xk: xkImage,
+  ye: yeImage,
+  yt: ytImage,
+  za: zaImage,
+  zm: zmImage,
+  zw: zwImage,
+};
+
+export default countries;
