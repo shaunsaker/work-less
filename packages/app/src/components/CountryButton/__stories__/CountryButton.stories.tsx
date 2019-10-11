189804ed1952df314f712e57aae13a38ce56095b
Shaun Saker <shaun@aux.co.za>
2019-10-11 16:55:32 +0200

    App/Storybook/Add withResponsiveViews

--- packages/app/src/components/CountryButton/__stories__/CountryButton.stories.tsx
+++ packages/app/src/components/CountryButton/__stories__/CountryButton.stories.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { storiesOf } from '@storybook/react'; // eslint-disable-line
 
 import CountryButton from '..';
+import withResponsiveViews from '../../../../.storybook/decorators/withResponsiveViews';
 
 const name = 'South Africa';
 const id = 'za';
 
-storiesOf('Components|CountryButton', module).add('default', () => (
-  <CountryButton name={name} id={id} handlePress={console.log} />
-));
+storiesOf('Components|CountryButton', module)
+  .addDecorator(withResponsiveViews)
+  .add('default', () => <CountryButton name={name} id={id} handlePress={console.log} />);
