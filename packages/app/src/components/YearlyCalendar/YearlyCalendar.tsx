e8446cc9281ab06cb285d56863d6e45fa3658bcb
Shaun Saker <shaun@aux.co.za>
2019-10-13 15:33:11 +0200

    YearlyCalendar/Add logic

--- packages/app/src/components/YearlyCalendar/YearlyCalendar.tsx
+++ packages/app/src/components/YearlyCalendar/YearlyCalendar.tsx
@@ -3,10 +3,28 @@ import { View } from 'reactxp';
 
 import styles from './styles';
 
-interface Props {}
+import Calendar from './Calendar';
+import { DateObject } from './Calendar';
 
-const YearlyCalendar: React.FC<Props> = () => {
-  return <View style={styles.container} />;
+interface Props {
+  months: Date[];
+  datesOfSignificance: DateObject[];
+}
+
+const YearlyCalendar: React.FC<Props> = ({ months, datesOfSignificance }) => {
+  return (
+    <View style={styles.container}>
+      {months.map((date) => {
+        const key = date.getTime();
+
+        return (
+          <View key={key} style={styles.calendarContainer}>
+            <Calendar date={date} datesOfSignificance={datesOfSignificance} />
+          </View>
+        );
+      })}
+    </View>
+  );
 };
 
 export default YearlyCalendar;
