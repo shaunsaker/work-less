c2e7c968053948fdfeece0b2cb074fca5f2dc007
Shaun Saker <shaun@aux.co.za>
2019-10-13 07:25:50 +0200

    Add blank YearlyCalendar

---
+++ packages/app/src/components/YearlyCalendar/YearlyCalendar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'reactxp';
+
+import styles from './styles';
+
+interface Props {}
+
+const YearlyCalendar: React.FC<Props> = ({}) => {
+  return <View style={styles.container} />;
+};
+
+export default YearlyCalendar;
---
+++ packages/app/src/components/YearlyCalendar/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import YearlyCalendar from './YearlyCalendar';
+
+interface Props {}
+
+const YearlyCalendarContainer: React.FC<Props> = (props) => {
+  return <YearlyCalendar />;
+};
+
+export default YearlyCalendarContainer;
