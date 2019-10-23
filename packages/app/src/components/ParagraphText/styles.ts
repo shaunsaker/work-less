004f89a02911c5ff27befe7f6b4926f2cb59f25c
Shaun Saker <shaun@aux.co.za>
2019-10-23 08:18:58 +0200

    Add ParagraphText component

---
+++ packages/app/src/components/ParagraphText/styles.ts
@@ -0,0 +1,13 @@
+import { Styles } from 'reactxp';
+
+import { colors } from '../../styleConstants';
+
+const styles = {
+  text: Styles.createTextStyle({
+    fontFamily: 'Roboto-Regular',
+    fontSize: 16,
+    color: colors.primaryText,
+  }),
+};
+
+export default styles;
