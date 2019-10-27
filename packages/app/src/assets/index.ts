986120fdf06ef1349ffe59651d2e873b67c16d11
Shaun Saker <shaun@aux.co.za>
2019-10-27 08:15:26 +0200

    App/Fix assets

--- packages/app/src/assets/index.ts
+++ packages/app/src/assets/index.ts
@@ -4,7 +4,7 @@ export interface IAssets {
   logo: string;
 }
 
-const getImageUrl = (image: string) => `${DOMAIN}/assets/images/${image}`;
+const getImageUrl = (image: string) => `${DOMAIN}/assets/images/${image}.png`;
 
 const Assets: IAssets = {
   logo: getImageUrl('logo'),
