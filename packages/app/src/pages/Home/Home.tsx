b4b0c1036407d06760f5d90549ecf123230a33bb
Shaun Saker <shaun@aux.co.za>
2019-10-27 07:57:55 +0200

    Home/Fix styles

--- packages/app/src/pages/Home/Home.tsx
+++ packages/app/src/pages/Home/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, ScrollView } from 'reactxp';
+import { View } from 'reactxp';
 
 import styles from './styles';
 
@@ -21,38 +21,36 @@ const Home: React.FC<Props> = ({ handleSubmitPress, handleFooterLinkPress }) => 
     <View style={styles.container}>
       <HeaderBar />
 
-      <ScrollView style={styles.contentWrapper}>
-        <View style={styles.contentContainer}>
-          <View style={styles.textContainer}>
-            <View style={styles.titleTextContainer}>
-              <TitleText
-                textArray={[
-                  { text: 'Maximise your ' },
-                  { text: 'consecutive time off', isHighlighted: true },
-                  { text: '.' },
-                ]}
-              />
-            </View>
-
-            <View style={styles.paragraphTextContainer}>
-              <ParagraphText>
-                Our app will auto-magically calculate the best configuration of leave days to
-                maximise your consecutive time off, based on your country’s leave days.
-                {'\n\n'}
-                Also, it’s absolutely free. Sweet deal!
-              </ParagraphText>
-            </View>
-
-            <View style={styles.buttonContainer}>
-              <Button text="LET'S GET CRACKALACKING" handlePress={handleSubmitPress} />
-            </View>
+      <View style={styles.contentContainer}>
+        <View style={styles.textContainer}>
+          <View style={styles.titleTextContainer}>
+            <TitleText
+              textArray={[
+                { text: 'Maximise your ' },
+                { text: 'consecutive time off', isHighlighted: true },
+                { text: '.' },
+              ]}
+            />
           </View>
-          {/* 
+
+          <View style={styles.paragraphTextContainer}>
+            <ParagraphText>
+              Our app will auto-magically calculate the best configuration of leave days to maximise
+              your consecutive time off, based on your country’s leave days.
+              {'\n\n'}
+              Also, it’s absolutely free. Sweet deal!
+            </ParagraphText>
+          </View>
+
+          <View style={styles.buttonContainer}>
+            <Button text="LET'S GET CRACKALACKING" handlePress={handleSubmitPress} />
+          </View>
+        </View>
+        {/* 
           <View style={styles.imageContainer}>
             <Image source={IMAGE} resizeMode="contain" style={styles.image} />
           </View> */}
-        </View>
-      </ScrollView>
+      </View>
 
       <FooterBar handleLinkPress={handleFooterLinkPress} />
     </View>
