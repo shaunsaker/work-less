94e813b7cdaa1ab26020cfd7999a5e67ac0ebf42
Shaun Saker <shaun@aux.co.za>
2019-11-02 07:54:58 +0200

    App/Setup jest

--- packages/app/jest/jest.config.js
+++ packages/app/jest/jest.config.js
@@ -2,19 +2,13 @@ module.exports = {
   rootDir: '../',
   roots: ['<rootDir>/src'],
 
-  moduleFileExtensions: [
-    'ts', 'tsx', 'js', 'json', 'node',
-  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json', 'node'],
 
   moduleDirectories: ['node_modules'],
 
-  snapshotSerializers: [
-    'enzyme-to-json/serializer',
-  ],
+  // snapshotSerializers: ['enzyme-to-json/serializer'],
 
-  setupFiles: [
-    '<rootDir>/jest/enzyme.config.js',
-  ],
+  // setupFiles: ['<rootDir>/jest/enzyme.config.js'],
 
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.((t|j)sx?)$',
 };
