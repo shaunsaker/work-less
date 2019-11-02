a8d0ffeca1747491db0a9e2475600c80a6de7d37
Shaun Saker <shaun@aux.co.za>
2019-11-02 07:12:23 +0200

    Home/Update UI

--- packages/app/src/pages/Home/Home.tsx
+++ packages/app/src/pages/Home/Home.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import { View } from 'reactxp';
+import { View, Image } from 'reactxp';
 
 import styles from './styles';
+import ASSETS from '../../assets';
 
 import HeaderBar from '../../components/HeaderBar';
 import TitleText from '../../components/TitleText';
 import ParagraphText from '../../components/ParagraphText';
 import Button from '../../components/Button';
 import FooterBar from '../../components/FooterBar';
-
-// const IMAGE = require('../../assets/images/home.png');
 
 interface Props {
   handleSubmitPress: () => void;
@@ -46,10 +45,10 @@ const Home: React.FC<Props> = ({ handleSubmitPress, handleFooterLinkPress }) => 
             <Button text="LET'S GET CRACKALACKING" handlePress={handleSubmitPress} />
           </View>
         </View>
-        {/* 
-          <View style={styles.imageContainer}>
-            <Image source={IMAGE} resizeMode="contain" style={styles.image} />
-          </View> */}
+
+        <View style={styles.imageContainer}>
+          <Image source={ASSETS.home} resizeMode="contain" style={styles.image} />
+        </View>
       </View>
 
       <FooterBar handleLinkPress={handleFooterLinkPress} />
