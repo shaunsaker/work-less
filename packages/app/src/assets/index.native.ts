7d11c5310778a1da76da455b542c652ae8a9b7dc
Shaun Saker <shaun@aux.co.za>
2019-11-03 13:08:53 +0200

    App/Add images

--- packages/app/src/assets/index.native.ts
+++ packages/app/src/assets/index.native.ts
@@ -2,10 +2,16 @@ import { IAssets } from '.';
 
 const LOGO = require('./images/logo.png');
 const HOME = require('./images/home.png');
+const SELECT_COUNTRY = require('./images/select-country.png');
+const INPUT_LEAVE_DAYS = require('./images/input-leave-days.png');
+const RESULTS = require('./images/results.png');
 
 const Assets: IAssets = {
   logo: LOGO,
   home: HOME,
+  selectCountry: SELECT_COUNTRY,
+  inputLeaveDays: INPUT_LEAVE_DAYS,
+  results: RESULTS,
 };
 
 export default Assets;
--- packages/app/src/assets/index.ts
+++ packages/app/src/assets/index.ts
@@ -3,6 +3,9 @@ import { DOMAIN } from '../config';
 export interface IAssets {
   logo: string;
   home: string;
+  selectCountry: string;
+  inputLeaveDays: string;
+  results: string;
 }
 
 const getImageUrl = (image: string) => `${DOMAIN}/assets/images/${image}.png`;
@@ -10,6 +13,9 @@ const getImageUrl = (image: string) => `${DOMAIN}/assets/images/${image}.png`;
 const Assets: IAssets = {
   logo: getImageUrl('logo'),
   home: getImageUrl('home'),
+  selectCountry: getImageUrl('select-country'),
+  inputLeaveDays: getImageUrl('input-leave-days'),
+  results: getImageUrl('results'),
 };
 
 export default Assets;
