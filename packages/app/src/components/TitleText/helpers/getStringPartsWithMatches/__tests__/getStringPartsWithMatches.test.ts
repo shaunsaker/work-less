d6e610f83ed7ac6d56e8dc7bbb763e37331ef580
Shaun Saker <shaun@aux.co.za>
2019-11-03 11:34:22 +0200

    App/getStringPartsWithMatches/Update tests

--- packages/app/src/components/TitleText/helpers/getStringPartsWithMatches/__tests__/getStringPartsWithMatches.test.ts
+++ packages/app/src/components/TitleText/helpers/getStringPartsWithMatches/__tests__/getStringPartsWithMatches.test.ts
@@ -4,12 +4,20 @@ describe('getStringPartsWithMatches()', () => {
   const string = 'This is a string that I am going to test for matches';
   const match = 'g that I am g';
 
-  it('should work correctly', () => {
+  it('should work correctly with a match', () => {
     const stringPartsWithMatches = getStringPartsWithMatches({ string, match });
 
     expect(stringPartsWithMatches.length).toEqual(3);
     expect(stringPartsWithMatches[0].isMatch).toBeUndefined();
     expect(stringPartsWithMatches[1].isMatch).toEqual(true);
     expect(stringPartsWithMatches[2].isMatch).toBeUndefined();
+  });
+
+  it('should work correctly with no match', () => {
+    const noMatch = 'zzzzz zzz zzz';
+    const stringPartsWithMatches = getStringPartsWithMatches({ string, match: noMatch });
+
+    expect(stringPartsWithMatches.length).toEqual(1);
+    expect(stringPartsWithMatches[0].isMatch).toBeUndefined();
   });
 });
