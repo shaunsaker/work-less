acce5b2356bd4645b3e98248e0db70b66bb1612e
Shaun Saker <shaun@aux.co.za>
2019-11-07 08:25:24 +0200

    AssetsGenerator/Update logic

--- packages/app/src/assets/index.native.ts
+++ packages/app/src/assets/index.native.ts
@@ -1,17 +1,519 @@
-import { Assets } from '.';
+import Assets from './Assets'
 
-const LOGO = require('./images/logo.png');
+const AD = require('./images/countries/ad.png');
+const AE = require('./images/countries/ae.png');
+const AF = require('./images/countries/af.png');
+const AG = require('./images/countries/ag.png');
+const AI = require('./images/countries/ai.png');
+const AL = require('./images/countries/al.png');
+const AM = require('./images/countries/am.png');
+const AN = require('./images/countries/an.png');
+const AO = require('./images/countries/ao.png');
+const AQ = require('./images/countries/aq.png');
+const AR = require('./images/countries/ar.png');
+const AS = require('./images/countries/as.png');
+const AT = require('./images/countries/at.png');
+const AU = require('./images/countries/au.png');
+const AW = require('./images/countries/aw.png');
+const AX = require('./images/countries/ax.png');
+const AZ = require('./images/countries/az.png');
+const BA = require('./images/countries/ba.png');
+const BB = require('./images/countries/bb.png');
+const BD = require('./images/countries/bd.png');
+const BE = require('./images/countries/be.png');
+const BF = require('./images/countries/bf.png');
+const BG = require('./images/countries/bg.png');
+const BH = require('./images/countries/bh.png');
+const BI = require('./images/countries/bi.png');
+const BJ = require('./images/countries/bj.png');
+const BL = require('./images/countries/bl.png');
+const BM = require('./images/countries/bm.png');
+const BN = require('./images/countries/bn.png');
+const BO = require('./images/countries/bo.png');
+const BQ = require('./images/countries/bq.png');
+const BR = require('./images/countries/br.png');
+const BS = require('./images/countries/bs.png');
+const BT = require('./images/countries/bt.png');
+const BV = require('./images/countries/bv.png');
+const BW = require('./images/countries/bw.png');
+const BY = require('./images/countries/by.png');
+const BZ = require('./images/countries/bz.png');
+const CA = require('./images/countries/ca.png');
+const CC = require('./images/countries/cc.png');
+const CD = require('./images/countries/cd.png');
+const CF = require('./images/countries/cf.png');
+const CG = require('./images/countries/cg.png');
+const CH = require('./images/countries/ch.png');
+const CI = require('./images/countries/ci.png');
+const CK = require('./images/countries/ck.png');
+const CL = require('./images/countries/cl.png');
+const CM = require('./images/countries/cm.png');
+const CN = require('./images/countries/cn.png');
+const CO = require('./images/countries/co.png');
+const CR = require('./images/countries/cr.png');
+const CU = require('./images/countries/cu.png');
+const CV = require('./images/countries/cv.png');
+const CW = require('./images/countries/cw.png');
+const CX = require('./images/countries/cx.png');
+const CY = require('./images/countries/cy.png');
+const CZ = require('./images/countries/cz.png');
+const DE = require('./images/countries/de.png');
+const DJ = require('./images/countries/dj.png');
+const DK = require('./images/countries/dk.png');
+const DM = require('./images/countries/dm.png');
+const DO = require('./images/countries/do.png');
+const DZ = require('./images/countries/dz.png');
+const EC = require('./images/countries/ec.png');
+const EE = require('./images/countries/ee.png');
+const EG = require('./images/countries/eg.png');
+const EH = require('./images/countries/eh.png');
+const ER = require('./images/countries/er.png');
+const ES = require('./images/countries/es.png');
+const ET = require('./images/countries/et.png');
+const FI = require('./images/countries/fi.png');
+const FJ = require('./images/countries/fj.png');
+const FK = require('./images/countries/fk.png');
+const FM = require('./images/countries/fm.png');
+const FO = require('./images/countries/fo.png');
+const FR = require('./images/countries/fr.png');
+const GA = require('./images/countries/ga.png');
+const GB = require('./images/countries/gb.png');
+const GD = require('./images/countries/gd.png');
+const GE = require('./images/countries/ge.png');
+const GF = require('./images/countries/gf.png');
+const GG = require('./images/countries/gg.png');
+const GH = require('./images/countries/gh.png');
+const GI = require('./images/countries/gi.png');
+const GL = require('./images/countries/gl.png');
+const GM = require('./images/countries/gm.png');
+const GN = require('./images/countries/gn.png');
+const GP = require('./images/countries/gp.png');
+const GQ = require('./images/countries/gq.png');
+const GR = require('./images/countries/gr.png');
+const GS = require('./images/countries/gs.png');
+const GT = require('./images/countries/gt.png');
+const GU = require('./images/countries/gu.png');
+const GW = require('./images/countries/gw.png');
+const GY = require('./images/countries/gy.png');
+const HK = require('./images/countries/hk.png');
+const HM = require('./images/countries/hm.png');
+const HN = require('./images/countries/hn.png');
+const HR = require('./images/countries/hr.png');
+const HT = require('./images/countries/ht.png');
+const HU = require('./images/countries/hu.png');
+const ID = require('./images/countries/id.png');
+const IE = require('./images/countries/ie.png');
+const IL = require('./images/countries/il.png');
+const IM = require('./images/countries/im.png');
+const IN = require('./images/countries/in.png');
+const IO = require('./images/countries/io.png');
+const IQ = require('./images/countries/iq.png');
+const IR = require('./images/countries/ir.png');
+const IS = require('./images/countries/is.png');
+const IT = require('./images/countries/it.png');
+const JE = require('./images/countries/je.png');
+const JM = require('./images/countries/jm.png');
+const JO = require('./images/countries/jo.png');
+const JP = require('./images/countries/jp.png');
+const KE = require('./images/countries/ke.png');
+const KG = require('./images/countries/kg.png');
+const KH = require('./images/countries/kh.png');
+const KI = require('./images/countries/ki.png');
+const KM = require('./images/countries/km.png');
+const KN = require('./images/countries/kn.png');
+const KP = require('./images/countries/kp.png');
+const KR = require('./images/countries/kr.png');
+const KW = require('./images/countries/kw.png');
+const KY = require('./images/countries/ky.png');
+const KZ = require('./images/countries/kz.png');
+const LA = require('./images/countries/la.png');
+const LB = require('./images/countries/lb.png');
+const LC = require('./images/countries/lc.png');
+const LI = require('./images/countries/li.png');
+const LK = require('./images/countries/lk.png');
+const LR = require('./images/countries/lr.png');
+const LS = require('./images/countries/ls.png');
+const LT = require('./images/countries/lt.png');
+const LU = require('./images/countries/lu.png');
+const LV = require('./images/countries/lv.png');
+const LY = require('./images/countries/ly.png');
+const MA = require('./images/countries/ma.png');
+const MC = require('./images/countries/mc.png');
+const MD = require('./images/countries/md.png');
+const ME = require('./images/countries/me.png');
+const MF = require('./images/countries/mf.png');
+const MG = require('./images/countries/mg.png');
+const MH = require('./images/countries/mh.png');
+const MK = require('./images/countries/mk.png');
+const ML = require('./images/countries/ml.png');
+const MM = require('./images/countries/mm.png');
+const MN = require('./images/countries/mn.png');
+const MO = require('./images/countries/mo.png');
+const MP = require('./images/countries/mp.png');
+const MQ = require('./images/countries/mq.png');
+const MR = require('./images/countries/mr.png');
+const MS = require('./images/countries/ms.png');
+const MT = require('./images/countries/mt.png');
+const MU = require('./images/countries/mu.png');
+const MV = require('./images/countries/mv.png');
+const MW = require('./images/countries/mw.png');
+const MX = require('./images/countries/mx.png');
+const MY = require('./images/countries/my.png');
+const MZ = require('./images/countries/mz.png');
+const NA = require('./images/countries/na.png');
+const NC = require('./images/countries/nc.png');
+const NE = require('./images/countries/ne.png');
+const NF = require('./images/countries/nf.png');
+const NG = require('./images/countries/ng.png');
+const NI = require('./images/countries/ni.png');
+const NL = require('./images/countries/nl.png');
+const NO = require('./images/countries/no.png');
+const NP = require('./images/countries/np.png');
+const NR = require('./images/countries/nr.png');
+const NU = require('./images/countries/nu.png');
+const NZ = require('./images/countries/nz.png');
+const OM = require('./images/countries/om.png');
+const PA = require('./images/countries/pa.png');
+const PE = require('./images/countries/pe.png');
+const PF = require('./images/countries/pf.png');
+const PG = require('./images/countries/pg.png');
+const PH = require('./images/countries/ph.png');
+const PK = require('./images/countries/pk.png');
+const PL = require('./images/countries/pl.png');
+const PM = require('./images/countries/pm.png');
+const PN = require('./images/countries/pn.png');
+const PR = require('./images/countries/pr.png');
+const PS = require('./images/countries/ps.png');
+const PT = require('./images/countries/pt.png');
+const PW = require('./images/countries/pw.png');
+const PY = require('./images/countries/py.png');
+const QA = require('./images/countries/qa.png');
+const RE = require('./images/countries/re.png');
+const RO = require('./images/countries/ro.png');
+const RS = require('./images/countries/rs.png');
+const RU = require('./images/countries/ru.png');
+const RW = require('./images/countries/rw.png');
+const SA = require('./images/countries/sa.png');
+const SB = require('./images/countries/sb.png');
+const SC = require('./images/countries/sc.png');
+const SD = require('./images/countries/sd.png');
+const SE = require('./images/countries/se.png');
+const SG = require('./images/countries/sg.png');
+const SH = require('./images/countries/sh.png');
+const SI = require('./images/countries/si.png');
+const SJ = require('./images/countries/sj.png');
+const SK = require('./images/countries/sk.png');
+const SL = require('./images/countries/sl.png');
+const SM = require('./images/countries/sm.png');
+const SN = require('./images/countries/sn.png');
+const SO = require('./images/countries/so.png');
+const SR = require('./images/countries/sr.png');
+const SS = require('./images/countries/ss.png');
+const ST = require('./images/countries/st.png');
+const SV = require('./images/countries/sv.png');
+const SX = require('./images/countries/sx.png');
+const SY = require('./images/countries/sy.png');
+const SZ = require('./images/countries/sz.png');
+const TC = require('./images/countries/tc.png');
+const TD = require('./images/countries/td.png');
+const TF = require('./images/countries/tf.png');
+const TG = require('./images/countries/tg.png');
+const TH = require('./images/countries/th.png');
+const TJ = require('./images/countries/tj.png');
+const TK = require('./images/countries/tk.png');
+const TL = require('./images/countries/tl.png');
+const TM = require('./images/countries/tm.png');
+const TN = require('./images/countries/tn.png');
+const TO = require('./images/countries/to.png');
+const TR = require('./images/countries/tr.png');
+const TT = require('./images/countries/tt.png');
+const TV = require('./images/countries/tv.png');
+const TW = require('./images/countries/tw.png');
+const TZ = require('./images/countries/tz.png');
+const UA = require('./images/countries/ua.png');
+const UG = require('./images/countries/ug.png');
+const UM = require('./images/countries/um.png');
+const US = require('./images/countries/us.png');
+const UY = require('./images/countries/uy.png');
+const UZ = require('./images/countries/uz.png');
+const VA = require('./images/countries/va.png');
+const VC = require('./images/countries/vc.png');
+const VE = require('./images/countries/ve.png');
+const VG = require('./images/countries/vg.png');
+const VI = require('./images/countries/vi.png');
+const VN = require('./images/countries/vn.png');
+const VU = require('./images/countries/vu.png');
+const WF = require('./images/countries/wf.png');
+const WS = require('./images/countries/ws.png');
+const XK = require('./images/countries/xk.png');
+const YE = require('./images/countries/ye.png');
+const YT = require('./images/countries/yt.png');
+const ZA = require('./images/countries/za.png');
+const ZM = require('./images/countries/zm.png');
+const ZW = require('./images/countries/zw.png');
 const HOME = require('./images/home.png');
-const SELECT_COUNTRY = require('./images/select-country.png');
 const INPUT_LEAVE_DAYS = require('./images/input-leave-days.png');
+const LOGO = require('./images/logo.png');
 const RESULTS = require('./images/results.png');
+const SELECT_COUNTRY = require('./images/select-country.png');
 
 const Assets: Assets = {
-  logo: LOGO,
-  home: HOME,
-  selectCountry: SELECT_COUNTRY,
-  inputLeaveDays: INPUT_LEAVE_DAYS,
-  results: RESULTS,
-};
+'ad': AD,
+'ae': AE,
+'af': AF,
+'ag': AG,
+'ai': AI,
+'al': AL,
+'am': AM,
+'an': AN,
+'ao': AO,
+'aq': AQ,
+'ar': AR,
+'as': AS,
+'at': AT,
+'au': AU,
+'aw': AW,
+'ax': AX,
+'az': AZ,
+'ba': BA,
+'bb': BB,
+'bd': BD,
+'be': BE,
+'bf': BF,
+'bg': BG,
+'bh': BH,
+'bi': BI,
+'bj': BJ,
+'bl': BL,
+'bm': BM,
+'bn': BN,
+'bo': BO,
+'bq': BQ,
+'br': BR,
+'bs': BS,
+'bt': BT,
+'bv': BV,
+'bw': BW,
+'by': BY,
+'bz': BZ,
+'ca': CA,
+'cc': CC,
+'cd': CD,
+'cf': CF,
+'cg': CG,
+'ch': CH,
+'ci': CI,
+'ck': CK,
+'cl': CL,
+'cm': CM,
+'cn': CN,
+'co': CO,
+'cr': CR,
+'cu': CU,
+'cv': CV,
+'cw': CW,
+'cx': CX,
+'cy': CY,
+'cz': CZ,
+'de': DE,
+'dj': DJ,
+'dk': DK,
+'dm': DM,
+'do': DO,
+'dz': DZ,
+'ec': EC,
+'ee': EE,
+'eg': EG,
+'eh': EH,
+'er': ER,
+'es': ES,
+'et': ET,
+'fi': FI,
+'fj': FJ,
+'fk': FK,
+'fm': FM,
+'fo': FO,
+'fr': FR,
+'ga': GA,
+'gb': GB,
+'gd': GD,
+'ge': GE,
+'gf': GF,
+'gg': GG,
+'gh': GH,
+'gi': GI,
+'gl': GL,
+'gm': GM,
+'gn': GN,
+'gp': GP,
+'gq': GQ,
+'gr': GR,
+'gs': GS,
+'gt': GT,
+'gu': GU,
+'gw': GW,
+'gy': GY,
+'hk': HK,
+'hm': HM,
+'hn': HN,
+'hr': HR,
+'ht': HT,
+'hu': HU,
+'id': ID,
+'ie': IE,
+'il': IL,
+'im': IM,
+'in': IN,
+'io': IO,
+'iq': IQ,
+'ir': IR,
+'is': IS,
+'it': IT,
+'je': JE,
+'jm': JM,
+'jo': JO,
+'jp': JP,
+'ke': KE,
+'kg': KG,
+'kh': KH,
+'ki': KI,
+'km': KM,
+'kn': KN,
+'kp': KP,
+'kr': KR,
+'kw': KW,
+'ky': KY,
+'kz': KZ,
+'la': LA,
+'lb': LB,
+'lc': LC,
+'li': LI,
+'lk': LK,
+'lr': LR,
+'ls': LS,
+'lt': LT,
+'lu': LU,
+'lv': LV,
+'ly': LY,
+'ma': MA,
+'mc': MC,
+'md': MD,
+'me': ME,
+'mf': MF,
+'mg': MG,
+'mh': MH,
+'mk': MK,
+'ml': ML,
+'mm': MM,
+'mn': MN,
+'mo': MO,
+'mp': MP,
+'mq': MQ,
+'mr': MR,
+'ms': MS,
+'mt': MT,
+'mu': MU,
+'mv': MV,
+'mw': MW,
+'mx': MX,
+'my': MY,
+'mz': MZ,
+'na': NA,
+'nc': NC,
+'ne': NE,
+'nf': NF,
+'ng': NG,
+'ni': NI,
+'nl': NL,
+'no': NO,
+'np': NP,
+'nr': NR,
+'nu': NU,
+'nz': NZ,
+'om': OM,
+'pa': PA,
+'pe': PE,
+'pf': PF,
+'pg': PG,
+'ph': PH,
+'pk': PK,
+'pl': PL,
+'pm': PM,
+'pn': PN,
+'pr': PR,
+'ps': PS,
+'pt': PT,
+'pw': PW,
+'py': PY,
+'qa': QA,
+'re': RE,
+'ro': RO,
+'rs': RS,
+'ru': RU,
+'rw': RW,
+'sa': SA,
+'sb': SB,
+'sc': SC,
+'sd': SD,
+'se': SE,
+'sg': SG,
+'sh': SH,
+'si': SI,
+'sj': SJ,
+'sk': SK,
+'sl': SL,
+'sm': SM,
+'sn': SN,
+'so': SO,
+'sr': SR,
+'ss': SS,
+'st': ST,
+'sv': SV,
+'sx': SX,
+'sy': SY,
+'sz': SZ,
+'tc': TC,
+'td': TD,
+'tf': TF,
+'tg': TG,
+'th': TH,
+'tj': TJ,
+'tk': TK,
+'tl': TL,
+'tm': TM,
+'tn': TN,
+'to': TO,
+'tr': TR,
+'tt': TT,
+'tv': TV,
+'tw': TW,
+'tz': TZ,
+'ua': UA,
+'ug': UG,
+'um': UM,
+'us': US,
+'uy': UY,
+'uz': UZ,
+'va': VA,
+'vc': VC,
+'ve': VE,
+'vg': VG,
+'vi': VI,
+'vn': VN,
+'vu': VU,
+'wf': WF,
+'ws': WS,
+'xk': XK,
+'ye': YE,
+'yt': YT,
+'za': ZA,
+'zm': ZM,
+'zw': ZW,
+'home': HOME,
+'inputLeaveDays': INPUT_LEAVE_DAYS,
+'logo': LOGO,
+'results': RESULTS,
+'selectCountry': SELECT_COUNTRY,
+}
 
 export default Assets;
