54a5447078255a538be45c183f829eaab90d018a
Shaun Saker <shaun@aux.co.za>
2019-11-07 08:25:24 +0200

    AssetsGenerator/Update logic

--- packages/assets-generator/src/index.ts
+++ packages/assets-generator/src/index.ts
@@ -7,9 +7,16 @@ import * as fs from 'fs';
  * we can use remote URIs and on mobile we can use local URIs.
  * This is done because ReactXP does not have built in support for
  * local URIs on the web.
+ *
+ * NOTE:
+ *
+ * Asset keys are camelCased
+ * Assets should be saved in kebab-case
  */
-const initialDirectoryPath = path.join(__dirname, '../../app/src/assets/images');
-console.log(initialDirectoryPath);
+const imagesFilePath = '/images';
+const relativeFilePath = `/assets/${imagesFilePath}`;
+const initialDirectoryPath = path.join(__dirname, `../../app/src/${relativeFilePath}`);
+const fileType = '.png'; // TODO: Allow for any file type
 
 /*
  * We first need to collect all of the images (including subdirs)
@@ -17,52 +24,130 @@ console.log(initialDirectoryPath);
  * Then we can iterate over that to create our index files
  */
 interface File {
-  name: string;
-  path: string;
+  id: string;
+  relativePath: string;
 }
 
 const filesArray: File[] = [];
 
-const getFiles = (directoryPath: string) => {
-  fs.readdir(directoryPath, (error, files) => {
-    if (error) {
-      console.log(error);
-    } else {
-      files.forEach((fileName) => {
-        console.log(fileName);
+const getVariableName = (string: string) => {
+  const variableName = string.replace(/-/g, '_').toUpperCase();
+
+  return variableName;
+};
+
+const stringToCamelCase = (string: string) => {
+  return string
+    .replace(/(?:^\w|[A-Z]|\b\w)/g, function(word, index) {
+      return index == 0 ? word.toLowerCase() : word.toUpperCase();
+    })
+    .replace(/\s+/g, '');
+};
+
+const getKey = (string: string) => {
+  const key = stringToCamelCase(string.replace(/-/g, ' '));
+
+  return key;
+};
+
+const getFiles = async (directoryPath: string, recursing?: boolean) => {
+  try {
+    const files = await fs.readdirSync(directoryPath);
+
+    for (const fileName of files) {
+      /*
+       * Test if the file is a folder
+       * and recurse, passing the new dir in
+       */
+      const filePath = path.join(directoryPath, fileName);
+      const stats = fs.lstatSync(filePath);
+      const isDirectory = stats.isDirectory();
+
+      if (isDirectory) {
         /*
-         * Test if the file is a folder
-         * and recurse, passing the new dir in
+         * Recurse using the filePath as the directoryPath
          */
-        const filePath = path.join(directoryPath, fileName);
+        await getFiles(filePath, true);
+      } else {
+        const id = fileName.replace(fileType, '');
+        const relativePath = `${filePath.replace(initialDirectoryPath, '')}`;
+        const file = {
+          id,
+          relativePath,
+        };
 
-        fs.lstat(filePath, (error, stats) => {
-          if (error) {
-            console.log(error);
-          } else {
-            const isDirectory = stats.isDirectory();
-
-            if (isDirectory) {
-              /*
-               * Recurse using the filePath as the directoryPath
-               */
-              getFiles(filePath);
-            } else {
-              const file = {
-                name: fileName,
-                path: filePath, // TODO: Need the relative file path
-              };
-              console.log(file);
-
-              filesArray.push(file);
-            }
-          }
-        });
-      });
+        filesArray.push(file);
+      }
     }
-  });
+  } catch (error) {
+    console.log(error);
+  }
+
+  if (!recursing) {
+    /*
+     * We have all the files, let's create our native files
+     */
+    let index = '';
+    index += "import Assets from './Assets'\n";
+    index += "import { DOMAIN } from '../config'\n";
+    index += '\n';
+    index +=
+      'const getImageUrl = (relativePath: string) => `${DOMAIN}/images/${relativePath}.png`;\n';
+    index += '\n';
+    index += 'const Assets: Assets = {\n';
+
+    filesArray.forEach((file: File) => {
+      const { id, relativePath } = file;
+
+      index += `'${getKey(id)}': getImageUrl('${relativePath}'),\n`;
+    });
+
+    index += '}\n';
+    index += '\n';
+    index += 'export default Assets;\n';
+
+    let indexNative = '';
+    indexNative += "import Assets from './Assets'\n";
+    indexNative += '\n';
+
+    filesArray.forEach((file: File) => {
+      const { id, relativePath } = file;
+
+      indexNative += `const ${getVariableName(id)} = require('./images${relativePath}');\n`;
+    });
+
+    indexNative += '\n';
+    indexNative += 'const Assets: Assets = {\n';
+
+    filesArray.forEach((file: File) => {
+      const { id } = file;
+
+      indexNative += `'${getKey(id)}': ${getVariableName(id)},\n`;
+    });
+
+    indexNative += '}\n';
+    indexNative += '\n';
+    indexNative += 'export default Assets;\n';
+
+    let assets = '';
+    assets += 'interface Assets {\n';
+
+    filesArray.forEach((file: File) => {
+      const { id } = file;
+
+      assets += `'${getKey(id)}': string;\n`;
+    });
+
+    assets += '}\n';
+    assets += '\n';
+    assets += 'export default Assets;\n';
+
+    const assetsDirectoryPath = initialDirectoryPath.replace(imagesFilePath, '');
+
+    await fs.writeFileSync(`${assetsDirectoryPath}/index.ts`, index);
+    await fs.writeFileSync(`${assetsDirectoryPath}/index.native.ts`, indexNative);
+    await fs.writeFileSync(`${assetsDirectoryPath}/Assets.ts`, assets);
+  }
 };
 
 getFiles(initialDirectoryPath);
-
-// TODO: How do we wait for all files before continuing?
