9f53400132a1b85ceadf6dd869580be6984fe3d8
Shaun Saker <shaun@aux.co.za>
2019-11-08 07:15:42 +0200

    Layout/Fix styles

--- packages/app/src/components/Layout/styles.ts
+++ packages/app/src/components/Layout/styles.ts
@@ -14,6 +14,7 @@ const styles = {
   wrapper: Styles.createViewStyle({
     minHeight: WINDOW_HEIGHT,
     backgroundColor: 'white',
+    flex: 1,
   }),
   container: Styles.createScrollViewStyle({}),
   headerBarContainer: Styles.createViewStyle({
