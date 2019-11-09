9da3cb35b03bbcdcb4a820c34cb6361363b7f80d
Shaun Saker <shaun@aux.co.za>
2019-11-09 18:43:48 +0200

    SelectCountry/Pass handleBack into stories

--- packages/app/src/pages/SelectCountry/__stories__/SelectCountry.stories.tsx
+++ packages/app/src/pages/SelectCountry/__stories__/SelectCountry.stories.tsx
@@ -5,7 +5,8 @@ import SelectCountry from '..';
 import COUNTRIES from './countries';
 
 const handleNavigate = (link: string) => console.log(link);
+const handleBack = () => {};
 
 storiesOf('Pages|SelectCountry', module).add('default', () => (
-  <SelectCountry countries={COUNTRIES} handleNavigate={handleNavigate} />
+  <SelectCountry countries={COUNTRIES} handleNavigate={handleNavigate} handleBack={handleBack} />
 ));
