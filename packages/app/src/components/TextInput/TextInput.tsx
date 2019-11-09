ff4dbaa214e2558be47719b908ed4486830cf01b
Shaun Saker <shaun@aux.co.za>
2019-11-09 18:43:39 +0200

    TextInput/Remove animation

--- packages/app/src/components/TextInput/TextInput.tsx
+++ packages/app/src/components/TextInput/TextInput.tsx
@@ -1,51 +1,26 @@
 import React from 'react';
-import { View, TextInput as TextInputComponent, Types, Animated } from 'reactxp';
+import { View, TextInput as TextInputComponent } from 'reactxp';
 
 import styles from './styles';
 
 interface Props {
   label: string;
   value?: string;
-  labelContainerStyles: Types.AnimatedViewStyleRuleSet;
-  labelStyles: Types.AnimatedTextStyleRuleSet;
-  handleRef: (element: TextInputComponent) => void;
-  handleLabelPress: () => void;
   handleChangeText: (text: string) => void;
   handleSubmitEditing: () => void;
-  handleFocus: () => void;
-  handleBlur: () => void;
 }
 
-const TextInput: React.FC<Props> = ({
-  label,
-  value,
-  labelContainerStyles,
-  labelStyles,
-  handleRef,
-  handleLabelPress,
-  handleChangeText,
-  handleSubmitEditing,
-  handleFocus,
-  handleBlur,
-}) => {
+const TextInput: React.FC<Props> = ({ label, value, handleChangeText, handleSubmitEditing }) => {
   return (
     <View style={styles.container}>
       <TextInputComponent
-        ref={handleRef}
         value={value}
+        placeholder={label}
         style={styles.input}
         accessibilityLabel={label}
         onChangeText={handleChangeText}
         onSubmitEditing={handleSubmitEditing}
-        onFocus={handleFocus}
-        onBlur={handleBlur}
       />
-
-      <Animated.View style={[styles.labelWrapper, labelContainerStyles]} onPress={handleLabelPress}>
-        <View style={styles.labelContainer}>
-          <Animated.Text style={[styles.label, labelStyles]}>{label}</Animated.Text>
-        </View>
-      </Animated.View>
     </View>
   );
 };
