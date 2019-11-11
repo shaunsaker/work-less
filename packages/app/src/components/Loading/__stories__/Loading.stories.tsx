feb03a3a96e8b69ddc39cd4b7ad3a54cae3ccc30
Shaun Saker <shaun@aux.co.za>
2019-11-11 07:46:21 +0200

    App/Add Loading component

---
+++ packages/app/src/components/Loading/__stories__/Loading.stories.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react'; // eslint-disable-line
+
+import Loading from '..';
+
+storiesOf('Components|Loading', module).add('default', () => <Loading />);
