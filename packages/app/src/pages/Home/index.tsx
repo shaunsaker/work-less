546a59b35936ba65c6b23b265f95f8a70f050950
Shaun Saker <shaun@aux.co.za>
2019-11-13 11:32:20 +0200

    Home/Update logic

--- packages/app/src/pages/Home/index.tsx
+++ packages/app/src/pages/Home/index.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import { withRouter, RouteComponentProps } from 'react-router';
-import { useDispatch } from 'react-redux';
 
 import Home from './Home';
 import routes from '../../Router/routes';
 import Page from '../../types/Page';
-import { setSnackbarMessage } from '../../store/reducers/snackbar/actions';
 
 interface Props extends Page, RouteComponentProps {}
 
 const HomeContainer: React.FC<Props> = ({ history }) => {
-  const dispatch = useDispatch();
-
   const onSubmit = () => {
-    dispatch(setSnackbarMessage(Date.now().toString()));
+    history.push(routes.selectCountry.path);
   };
 
   return <Home handleSubmit={onSubmit} />;
