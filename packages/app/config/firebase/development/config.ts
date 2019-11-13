a04c6289cedf12bce2f029bdfdb0927aef525b54
Shaun Saker <shaun@aux.co.za>
2019-11-13 14:12:11 +0200

    Add Firebase web config

---
+++ packages/app/config/firebase/development/config.ts
@@ -0,0 +1,12 @@
+const config = {
+  apiKey: 'AIzaSyBS_AXGHzSReFkhywyziQzlVuLAEOpHtrM',
+  authDomain: 'work-less-development.firebaseapp.com',
+  databaseURL: 'https://work-less-development.firebaseio.com',
+  projectId: 'work-less-development',
+  storageBucket: 'work-less-development.appspot.com',
+  messagingSenderId: '722963579969',
+  appId: '1:722963579969:web:0c32fa462446c31e2e72c0',
+  measurementId: 'G-N812QC5RDP',
+};
+
+export default config;
---
+++ packages/app/config/firebase/production/config.ts
@@ -0,0 +1,12 @@
+const config = {
+  apiKey: 'AIzaSyAv9kphXCNAeYFJAMla3uC_Iva9GgnPSwk',
+  authDomain: 'work-less-production.firebaseapp.com',
+  databaseURL: 'https://work-less-production.firebaseio.com',
+  projectId: 'work-less-production',
+  storageBucket: 'work-less-production.appspot.com',
+  messagingSenderId: '540950562559',
+  appId: '1:540950562559:web:2bc448be39d353b6d3eeca',
+  measurementId: 'G-64RYLZQXD2',
+};
+
+export default config;
