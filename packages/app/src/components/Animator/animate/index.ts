9a7d3614ebb8384300cad440827ced90b797fb26
Shaun Saker <shaun@aux.co.za>
2019-11-13 06:54:33 +0200

    App/Update Animator component

--- packages/app/src/components/Animator/animate/index.ts
+++ packages/app/src/components/Animator/animate/index.ts
@@ -1,15 +1,18 @@
 import { Animated, Types } from 'reactxp';
 
+interface Args {
+  animatedValue: Types.AnimatedValue;
+  toValue: number;
+  duration: number;
+  useNativeDriver: boolean;
+  handleAnimateEnd?: () => void;
+}
+
 // NOTE: No test needed here since it's very close to the native function
-const animate = (
-  animatedValue: Types.AnimatedValue,
-  toValue: number,
-  useNativeDriver: boolean,
-  handleAnimateEnd?: () => void,
-) => {
+const animate = ({ animatedValue, toValue, duration, useNativeDriver, handleAnimateEnd }: Args) => {
   Animated.timing(animatedValue, {
     toValue,
-    duration: 250,
+    duration,
     easing: Animated.Easing.InOut(),
     useNativeDriver,
   }).start(handleAnimateEnd);
