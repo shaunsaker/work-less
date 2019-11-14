8417ae6404b0a727b0353816d434ed1a5fe81a71
Shaun Saker <shaun@aux.co.za>
2019-11-14 12:18:48 +0200

    App/Refactor store

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,17 +1,9 @@
 import React from 'react';
 
-import { model } from './model';
+import { Provider } from './model';
 import Router from './Router';
 import ErrorHandler from './containers/ErrorHandler';
 import SnackbarHandler from './containers/SnackbarHandler';
-
-const { Provider } = model.createStore({
-  logger: true,
-  initState: {
-    snackbar: {},
-    user: null,
-  },
-});
 
 const App: React.FC = () => {
   return (
