78d23f7fb27266a30403d07627ee853e7131a610
Shaun Saker <shaun@aux.co.za>
2019-11-14 12:18:48 +0200

    App/Refactor store

--- packages/app/src/model.ts
+++ packages/app/src/model.ts
@@ -11,4 +11,12 @@ export interface State {
 
 export const model = createModel<State>().with(loggerPlugin);
 
+export const { Provider } = model.createStore({
+  logger: true,
+  initState: {
+    snackbar: {},
+    user: null,
+  },
+});
+
 export const { state, watch, dispatch } = model.ctx;
