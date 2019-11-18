f4fe31d2dd53a9038b56e73cec9e131e44a13835
Shaun Saker <shaun@aux.co.za>
2019-11-18 08:21:46 +0200

    App/Fix calendar styles on small screens on the Results page

--- packages/app/src/pages/Results/styles.ts
+++ packages/app/src/pages/Results/styles.ts
@@ -1,6 +1,7 @@
 import { Styles } from 'reactxp';
 
 import { rhythm } from '../../styleConstants';
+import { getWindowResponsiveClass } from '../../utils';
 
 const styles = {
   container: Styles.createViewStyle({
@@ -8,7 +9,7 @@ const styles = {
   }),
   calendarContainer: Styles.createViewStyle({
     marginTop: rhythm.vt * -1, // remove margin from first nth calendar item styles
-    marginLeft: rhythm.hz * -1, // as above
+    marginLeft: getWindowResponsiveClass() === 'sm' ? 0 : rhythm.hz * -1, // as above
   }),
 };
 
